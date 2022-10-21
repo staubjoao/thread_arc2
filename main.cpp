e12555c2f806bc740884b2c43c034392dd90c27f
João Staub <joao.staub42@gmail.com>
2022-10-21 16:11:12 -0300

    comeco

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory>
+#include <thread>
+
+int fibo(int n)
+{
+    if (n <= 2)
+        return 1;
+    else
+        return fibo(n - 1) + fibo(n - 2);
+}
+
+class Worker
+{
+private:
+    int result;
+
+public:
+    Worker();
+    void task(int parameter);
+    void dump();
+};
+
+Worker::Worker()
+{
+    result = 0;
+}
+
+void Worker::task(int parameter)
+{
+    result = fibo(parameter);
+}
+
+void Worker::dump()
+{
+    std::cout << result << std::endl;
+}
+
+int main()
+{
+    Worker w1;
+    std::shared_ptr<Worker> w2(new Worker);
+
+    std::thread t1(&Worker::task, &w1, 30);
+}
