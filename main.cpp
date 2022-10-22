e83b3bac5fc06cd70816f3cb0766b00898157787
João Staub <joao.staub42@gmail.com>
2022-10-22 13:13:17 -0300

    soma usando thread, matriz 10x10

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <time.h>
 #include <memory>
 #include <thread>
 
-int fibo(int n)
+int somaVetor(int n, int *vet1, int *vet2)
 {
-    if (n <= 2)
-        return 1;
-    else
-        return fibo(n - 1) + fibo(n - 2);
+    int i, soma = 0;
+    for (i = 0; i < n; i++)
+        soma += (vet1[i] + vet2[i]);
+    return soma;
 }
 
 class Worker
@@ -17,8 +18,8 @@ private:
 
 public:
     Worker();
-    void task(int parameter);
-    void dump();
+    void taskSum(int n, int *v1, int *v2);
+    int getResult();
 };
 
 Worker::Worker()
@@ -26,28 +27,142 @@ Worker::Worker()
     result = 0;
 }
 
-void Worker::task(int parameter)
+void Worker::taskSum(int n, int *v1, int *v2)
 {
-    result = fibo(parameter);
+    result = somaVetor(n, v1, v2);
 }
 
-void Worker::dump()
+int Worker::getResult()
 {
-    std::cout << result << std::endl;
+    return result;
+}
+
+int somarMatriz(int n, int **matriz1, int **matriz2)
+{
+    int i, j, res = 0;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+            res += (matriz1[i][j] + matriz2[i][j]);
+    }
+    return res;
+}
+
+int **gerarMatriz(int n)
+{
+    int i, j;
+    int **matriz = new int *[n];
+    for (i = 0; i < n; i++)
+        matriz[i] = new int[n];
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+            matriz[i][j] = rand() % 99 + 1;
+    }
+    return matriz;
+}
+
+void apagarMatriz(int n, int **matriz)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        delete[] matriz[i];
+    delete[] matriz;
+}
+
+int somaThreads(int n, int **m1, int **m2)
+{
+    int res = 0, i;
+    for (i = 0; i < n; i += 5)
+    {
+        Worker w1;
+        Worker w2;
+        Worker w3;
+        Worker w4;
+        Worker w5;
+
+        std::thread t1(&Worker::taskSum, &w1, n, m1[i], m2[i]);
+        std::thread t2(&Worker::taskSum, &w2, n, m1[i + 1], m2[i + 1]);
+        std::thread t3(&Worker::taskSum, &w3, n, m1[i + 2], m2[i + 2]);
+        std::thread t4(&Worker::taskSum, &w4, n, m1[i + 3], m2[i + 3]);
+        std::thread t5(&Worker::taskSum, &w5, n, m1[i + 4], m2[i + 4]);
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        t5.join();
+
+        res += w1.getResult() + w2.getResult() + w3.getResult() + w4.getResult() + w5.getResult();
+    }
+    return res;
 }
 
 int main()
 {
-    Worker w1;
-    std::shared_ptr<Worker> w2(new Worker);
+    srand(time(NULL));
 
-    std::thread t1(&Worker::task, &w1, 30);
-    std::thread t2(&Worker::task, w2, 20);
+    double time_spent;
+    clock_t begin, end;
 
-    t1.join();
-    t2.join();
+    int n, i;
+    n = 10;
+    int **m1, **m2;
 
-    w1.dump();
-    w2->dump();
+    m1 = gerarMatriz(n);
+    m2 = gerarMatriz(n);
+    time_spent = 0.0;
+    begin = clock();
+    int somam1 = somarMatriz(n, m1, m2);
+    end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+
+    std::cout << "Resultado da soma sequencial: " << somam1 << "\nFeita no tempo: " << time_spent << std::endl;
+
+    time_spent = 0.0;
+    begin = clock();
+    int somat = somaThreads(n, m1, m2);
+    end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+
+    std::cout << "Resultado da soma usando threads: " << somat << "\nFeita no tempo: " << time_spent << std::endl;
+
+    apagarMatriz(n, m1);
+    apagarMatriz(n, m2);
+
+    // n = 100;
+    // int **m2 = new int *[n];
+    // for (i = 0; i < n; i++)
+    //     m2[i] = new int[n];
+
+    // gerarMatriz(n, m2);
+    // int somam2 = somarMatriz(n, m2);
+    // int subm2 = subtrairMatriz(n, m2);
+
+    // n = 1000;
+    // int **m3 = new int *[n];
+    // for (i = 0; i < n; i++)
+    //     m3[i] = new int[n];
+
+    // gerarMatriz(n, m3);
+    // int somam3 = somarMatriz(n, m3);
+    // int subm3 = subtrairMatriz(n, m3);
+
+    // n = 10;
+    // for (i = 0; i < n; i++)
+    //     delete[] m1[i];
+    // delete[] m1;
+
+    // n = 100;
+    // for (i = 0; i < n; i++)
+    //     delete[] m2[i];
+    // delete[] m2;
+
+    // n = 1000;
+    // for (i = 0; i < n; i++)
+    //     delete[] m3[i];
+    // delete[] m3;
+
     return 0;
 }
